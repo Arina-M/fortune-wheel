6fbd3cec2daec33eeb3c535229afd225c5034386
Arina-M <79450238+Arina-M@users.noreply.github.com>
2021-12-13 16:17:41 +0200

    update

--- wheel/script.js
+++ wheel/script.js
@@ -81,8 +81,95 @@ else {
 
 
 
-
+// to object? |
 //------------------------------------------------------------------------------------
+                   //discount | degrees | colour
+/*let discountArray = [['Скидка1', 60, '832c54'],
+                     ['Скидка2', 120, '627473'],
+                     ['Скидка3', 180, '34495e'],
+                     ['Скидка4', 240, 'e7e3e3'],
+                     ['Скидка5', 300, 'a85c7f'],
+                     ['Скидка6', 360, '293540']];
+
+
+
+let arrayDiv = [];
+
+
+function getDiv(){
+    discountArray.forEach(function(item, index){
+        arrayDiv.push(`<div class="discount${index+1}" >${item[0]}</div>`);
+    });
+    return arrayDiv.join('');
+}
+
+//todo
+function getStyle(){
+    // let divs = getDiv();
+    discountArray.forEach(function (item, index){
+        let div = document.querySelector(`.discount${index+1}`);
+        div.style.borderColor = `${item[2]} transparent`;
+        div.style.transform = `rotate:${item[1]}deg`;
+        // div.style.
+    });
+}
+
+getStyle();
+
+function mainHTML(){
+    let mainHTML = `
+    <div id="inner-wheel2">
+        <div class="sec">
+             ${getDiv()}
+        </div>
+    </div> 
+    `;
+
+    document.write(mainHTML);
+    console.log(mainHTML);
+
+    document.body.insertBefore(mainHTML, wheel);
+
+}
+
+
+mainHTML();
+*/
+/*
+let keyframes;
+keyframes.insertRule("0% {
+    -webkit-transform: translate(100px, 100px) rotate(" + (multiplier + 0) + "deg)
+translate(-100px, -100px) rotate(" + (multiplier + 0) + "deg);
+background-color: red;
+}");
+keyframes.insertRule("13% {
+    -webkit-transform: translate(100px, 100px) rotate(" + (multiplier + 45) + "deg)
+translate(-100px, -100px) rotate(" + (multiplier + 45) + "deg);
+}");
+
+
+// Gets the browser prefix
+let browserPrefix;
+let navigator;
+navigator.sayswho = (function(){
+    let N = navigator.appName, ua = navigator.userAgent, tem;
+    let M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    if(M && (tem = ua.match(/version\/([\.\d]+)/i))!= null) M[2] = tem[1];
+    M = M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+    M = M[0];
+    if(M === "Chrome") { browserPrefix = "webkit"; }
+    if(M === "Firefox") { browserPrefix = "moz"; }
+    if(M === "Safari") { browserPrefix = "webkit"; }
+    if(M === "MSIE") { browserPrefix = "ms"; }
+});
+*/
+
+
+
+let discountArr = [['-67% на персональные тренировки', 60],
+                   ['-73% на гостевой абонемент', 180],
+                   ['-50% на солярий', 240]];
+
 
 let clicks = 0;
 let degrees = 0;
@@ -90,44 +177,64 @@ let wheel = document.querySelector('#fortune-wheel');
 let play = document.querySelector('.fa-play-circle');
 let lock = document.querySelector('.fa-lock');
 
+
+//spinning
 function spinIt() {
-        clicks++;
-        //random degrees
-        degrees = Math.floor(500+ Math.random() * 5000);
-        console.log(degrees);
-        wheel.style.transition = "all 6s ease-out";
-        wheel.style.transform = `rotate(${degrees}deg)`;
-        console.log(clicks+" >>");
+    clicks++;
+    //random degrees
+    degrees = 2800;
+    console.log(degrees);
+    wheel.style.transition = "all 6s ease-out";
+    wheel.style.transform = `rotate(${degrees}deg)`;
+    wheel.style.animationFillMode = "both";
+    console.log(clicks + " >>");
 
     //static degrees
     //  wheel.style.animation = "spinRotation 3.5s";
     // wheel.style.animationFillMode = "both";
-    // wheel.style.transformOrigin = "center";x
+    // wheel.style.transformOrigin = "center";
 
 
     play.style.visibility = "hidden";
     lock.style.visibility = "visible";
-function jdu
+}
 
 
 
+
+
+
+
+/*
+function getDiscount(){
+    let arrayDis = [];
+    discountArray.forEach(function(item){
+        // console.log(item) ;
+        arrayDis.push(item[0]);
+    });
+     return arrayDis.join();
+}
+
+function getDegrees(){
+    let arrayDegr = [];
+    discountArray.forEach(function(item){
+        // console.log(item) ;
+        arrayDegr.push(item[1]);
+    });
+    return arrayDegr.join();
 
 }
 
-// console.log(window.getComputedStyle(document.querySelector('#inner-spin'), ':after'));
+function getColour(){
+    let arrayCol = [];
+    discountArray.forEach(function(item){
+        // console.log(item) ;
+        arrayCol.push(item[2]);
+    });
+    return arrayCol.join();
 
-
-// function setItemToLocaleStorage(){
-//
-// }
-//
-// function getItemFromLocaleStorage(){
-//     localeStorage.setItem();
-// }
-
-
-
-
+}
+*/
 
 
 
